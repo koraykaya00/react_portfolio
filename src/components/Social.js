ae4b327e0c87eaf2220c359d4369c9b434d4a7a9
Koray00 <i.korayykaya@gmail.com>
2023-03-15 16:17:40 +0300

    Social Commit

---
+++ src/components/Social.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Icon } from '@chakra-ui/react'
+import { HStack } from '@chakra-ui/layout';
+import { FaFacebookF, FaGoogle, FaSpotify, FaShopify} from 'react-icons/fa'
+
+function Social() {
+  return (
+    <HStack spacing='24'>
+    <Icon as= {FaFacebookF} boxSize='50' />
+    <Icon as={FaGoogle} boxSize='50' />
+    <Icon as={FaSpotify} boxSize='50' />
+    <Icon as={FaShopify} boxSize='50' />
+    </HStack>
+
+  )
+}
+
+export default Social
